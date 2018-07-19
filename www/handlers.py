0f22952412b7e1de50184b7f72a6aa77c3e719bc
siumennel <siumennel@gmail.com>
2018-07-19 15:29:26 +0900

    day6 day7 config and mvc

--- www/handlers.py
+++ www/handlers.py
@@ -1,6 +1,24 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 __author__ = 'Michael Liao'
 
 ' url handlers '
+
+import re
+import time
+import json
+import logging
+import hashlib
+import base64
+import asyncio
+
+from coroweb import get, post
+
+from models import User, Comment, Blog, next_id
+
+
+@get('/')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }
